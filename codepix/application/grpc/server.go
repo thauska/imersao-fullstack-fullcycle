bdb542e5bbb68cf918ebd4cc3c2377c3ce2b070c
Thayana Mamoré <thayana.mamore@topi.io>
2021-02-08 19:12:59 -0300

    for cmd

--- codepix/application/grpc/server.go
+++ codepix/application/grpc/server.go
@@ -6,22 +6,12 @@ import (
 	"net"
 
 	"github.com/jinzhu/gorm"
-	"github.com/thauska/imersao-fullstack-fullcycle/codepix/application/grpc/pb"
-	"github.com/thauska/imersao-fullstack-fullcycle/codepix/application/usecase"
-	"github.com/thauska/imersao-fullstack-fullcycle/codepix/infrastructure/repository"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 //StartGrpcServer create a new gRPC server
 func StartGrpcServer(database *gorm.DB, port int) {
 	grpcServer := grpc.NewServer()
-	reflection.Register(grpcServer)
-
-	pixRepository := repository.PixKeyRepositoryDb{Db: database}
-	pixUseCase := usecase.PixUseCase{PixKeyRepository: pixRepository}
-	pixGrpcService := NewPixGrpcService(pixUseCase)
-	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
 
 	address := fmt.Sprintf("0.0.0.0:%d", port)
 	listener, err := net.Listen("tcp", address)
